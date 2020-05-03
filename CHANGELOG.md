42ff91cc4e22db45af110c015a27f7afb7908352
vehsoriano <verrell.soriano09@gmail.com>
2020-05-03 22:30:21 +1000

    Updates

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+# Changelog
+### `9c2afb1`
+* update `react@^16.12.0`
+* update `react-dom@^16.12.0`
+* update `react-router-dom@^5.1.2`
+* update `react-scripts@^3.3.1`
+* remove `react-id-swiper` and `swiper`
+* update filename cases to follow convention
+* add `.vscode` workspace settings
+* add `.prettierc` in alignment with airbnb react style guide
--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import { hot } from 'react-hot-loader';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from './containers/Home';
+import NoRouteMatch from './containers/404';
+
+import './stylesheets/main.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <HashRouter basename="/">
+        <div className="App">
+
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route component={NoRouteMatch} />
+          </Switch>
+
+        </div>
+      </HashRouter>
+    );
+  }
 }
 
-export default App;
+export default hot(module)(App);
